9c792009d0ed5d6bcbc01cd224f6a7ab5d4624eb
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2021-02-12 17:42:01 -0500

    Initial implementation of receiving data from Yelp API

--- pages/SwipePage.js
+++ pages/SwipePage.js
@@ -1,43 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Restaurant from '../components/Restaurant';
-
 import Swiper from 'react-native-deck-swiper'
-import * as data from '../data/api-test-data.json';
+
+const url = 'http://localhost:3000/restaurant_data';
 
 export default function SwipePage() {
+    const [businesses, setBusinesses] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        try {
+            let results = await fetch(url, {
+                method: 'GET'
+            });
+
+            setBusinesses(await results.json());
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     function renderCard(card, index) {
         // Convert meters to kilometers and round to 1 decimal place
-        let distanceKm = Math.round((data['businesses'][index].distance / 1000) * 10) / 10;
+        let distanceKm = Math.round((businesses[index].distance / 1000) * 10) / 10;
 
         return (
-            <Restaurant 
-                images={data['businesses'][index].photos} 
-                name={data['businesses'][index].name} 
+            <Restaurant
+                images={businesses[index].photos}
+                name={businesses[index].name}
                 distance={distanceKm + ' Kilometers away'}
-                price={data['businesses'][index].price} 
-                description={data['businesses'][index].categories[0].title}/>
+                price={businesses[index].price}
+                description={businesses[index].categories[0].title} />
         )
     };
+
+    // Hide Swiper content until the GET request resturn data
+    const Hide = (props) => businesses.length > 0 ? <>{props.children}</> : <></>
 
     return (
         <View style={styles.container}>
             <StatusBar style="light" />
             <Header style={styles.header} />
             <View style={styles.swiper}>
-                <Swiper
-                    cards={[...data['businesses'].keys()]}
-                    renderCard={renderCard}
-                    backgroundColor={'#292929'}
-                    cardVerticalMargin={0}
-                    verticalSwipe={false}
-                    cardStyle={styles.card}
-                    marginBottom={10}
-                    stackSize={2}/>
-                </View>
+                <Hide>
+                    <Swiper
+                        cards={businesses}
+                        renderCard={renderCard}
+                        backgroundColor={'#292929'}
+                        cardVerticalMargin={0}
+                        verticalSwipe={false}
+                        cardStyle={styles.card}
+                        marginBottom={10}
+                        stackSize={2} />
+                </Hide>
+            </View>
             <Footer />
         </View>
     );
