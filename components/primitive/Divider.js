6429fd169fc46f5ee86b775a0791f4bd6948ef70
Ahmed Sakr <ahmed@sakr.ca>
2021-02-15 11:15:26 -0500

    move styles to separate object

--- components/primitive/Divider.js
+++ components/primitive/Divider.js
@@ -3,16 +3,17 @@ import { StyleSheet, View } from 'react-native';
 
 const Divider = (props) => {
     return (
-        <View style={{
-            height: props.height,
-            backgroundColor: '#fff',
-            width: "100%",
-            opacity: 0.6,
-        }} />
+        <View style={styles.divider} />
     );
 }
 
 const styles = StyleSheet.create({
+    divider: {
+        height: props.height,
+        backgroundColor: '#fff',
+        width: "100%",
+        opacity: 0.6,
+    }
 });
 
 export default Divider;
--- components/primitive/Textfield.js
+++ components/primitive/Textfield.js
@@ -3,20 +3,21 @@ import { StyleSheet, TextInput } from 'react-native';
 
 const Textfield = (props) => {
     return (
-        <TextInput placeholder={props.placeholder}  placeholderTextColor="#ccc" style={{
-            padding: 12,
-            borderColor: '#70EFDE',
-            borderWidth: 2,
-            borderRadius: 10,
-            width: "80%",
-            textAlign: 'center',
-            color: 'white',
-            fontSize: 36
-        }} />
+        <TextInput placeholder={props.placeholder} placeholderTextColor="#ccc" style={styles.input} />
     );
 }
 
 const styles = StyleSheet.create({
+    input: {
+        padding: 12,
+        borderColor: '#70EFDE',
+        borderWidth: 2,
+        borderRadius: 10,
+        width: "80%",
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 36
+    }
 });
 
 export default Textfield;
