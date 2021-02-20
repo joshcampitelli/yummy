9137262fa039a25f3a81ae58c4d9ced0aea543c4
MichaelCalabro <mmc.calabro@gmail.com>
2021-02-20 13:04:53 -0500

    revert appjs to swiper

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import SwipePage from './pages/SwipePage';
-import GuestLobby from './pages/GuestLobby';
-import HostLobby from './pages/HostLobby';
 
 export default function App() {
     return (
-       // <SwipePage />
-       //<GuestLobby/>
-       <HostLobby/>
+       <SwipePage />
     );
 }
