695f7c9741f5997cdf95d0ca67c1208c20ca5d78
Ahmed Sakr <ahmed@sakr.ca>
2021-07-12 06:32:11 -0400

    adjust constant names

--- store/fixtures.js
+++ store/fixtures.js
@@ -1,10 +1,10 @@
 // temporary user geolocation and settings until we implement them
-export const userGameSettings = {
+export const USER_GAME_SETTINGS = {
   latitude: 45.421532,
   longitude: -75.697189,
   radius: 20000,
   price: '1, 2, 3, 4',
 };
 
-const serverBase = 'http://192.168.0.96:3000'
-export const fetchEligibleRestaurantsEndpoint = `${serverBase}/restaurant_data`;
+const SERVER_BASE_URL = 'http://192.168.0.96:3000'
+export const FETCH_ELIGIBLE_RESTAURANTS_URL = `${SERVER_BASE_URL}/restaurant_data`;
--- store/swiping/swiping.actions.js
+++ store/swiping/swiping.actions.js
@@ -1,17 +1,17 @@
 import { FETCH_ELIGIBLE_RESTAURANTS } from './swiping.types';
-import { userGameSettings, fetchEligibleRestaurantsEndpoint } from '../fixtures';
+import { USER_GAME_SETTINGS, FETCH_ELIGIBLE_RESTAURANTS_URL } from '../fixtures';
 
 export const fetchEligibleRestaurants = async (
   offset = 0,
 ) => {
-  const results = await fetch(fetchEligibleRestaurantsEndpoint, {
+  const results = await fetch(FETCH_ELIGIBLE_RESTAURANTS_URL, {
     method: 'POST',
-    body: JSON.stringify({ ...userGameSettings, offset }),
+    body: JSON.stringify({ ...USER_GAME_SETTINGS, offset }),
     headers: { 'Content-type': 'application/json' },
   });
 
   const content = await results.json();
-  
+
   return {
     type: FETCH_ELIGIBLE_RESTAURANTS,
     payload: content,
